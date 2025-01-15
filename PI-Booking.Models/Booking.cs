ded4f37ec23e3d0c383b936bd19a96c54f19e2ab
Steve Kay <stephen.kay@methods.co.uk>
2025-01-15 10:06:42 +0000

    Added models project Added reference to QRCoder.Core Generator working

---
+++ PI-Booking.Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PI_Booking.Models
+{
+    public class Booking
+    {
+        public string? Reference { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
