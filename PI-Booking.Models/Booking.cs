f6c9faf73b9711ef31fa06b4ca2f1c7d5953e4f8
Steve Kay <stephen.kay@methods.co.uk>
2025-01-16 19:11:20 +0000

    Fixed single row form edit

--- PI-Booking.Models/Booking.cs
+++ PI-Booking.Models/Booking.cs
@@ -5,7 +5,7 @@ namespace PI_Booking.Models
     public class Booking
     {
         public string? Reference { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
     }
 }
